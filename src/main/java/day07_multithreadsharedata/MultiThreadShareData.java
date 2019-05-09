b26dbc23c9b866094fb525da99812f595069ffd3
lnongge <756725700@qq.com>
2019-05-10 01:01:47 +0800

    day09.线程池

--- src/main/java/day07_multithreadsharedata/MultiThreadShareData.java
+++ src/main/java/day07_multithreadsharedata/MultiThreadShareData.java
@@ -1,9 +1,11 @@
 package day07_multithreadsharedata;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * @Description: 多个线程共享变量演示一: 卖票
  *  一个线程就是一个卖票窗口,每个窗口干的活都一样是卖票
- *  如何解决线程安全问题? 待解决
+ *  如何解决线程安全问题? (待解决) 用day08的AtomicInteger--还是一样有线程问题,而如果加synchronize
  * @Author: lianws
  * @Date: 2019/5/7 7:59
  */
@@ -18,11 +20,13 @@ public class MultiThreadShareData {
 }
 
 class ShareData implements Runnable{
-    private int count=100; //初始1000张票
+//    private int count=100; //初始100张票
+    private AtomicInteger count=new AtomicInteger(100);
     @Override
     public  void run() {
-        while(count>0){
-            count--;
+        while(count.intValue()>0){
+//            count--;
+            count.decrementAndGet();
             System.out.println(Thread.currentThread().getName()+"卖出一张票,余票:"+count);
         }
         System.out.println(Thread.currentThread().getName()+"窗口的票卖光了...");
---
+++ src/main/java/day09_threadpool/ThreadPoolTest.java
@@ -0,0 +1,51 @@
+package day09_threadpool;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Description: 线程池demo
+ * 创建10个任务(就是Runable),1个任务执行10次
+ * @Author: lianws
+ * @Date: 2019/5/9 22:28
+ */
+public class ThreadPoolTest {
+    public static void main(String[] args) {
+        //1
+//        ExecutorService executorService = Executors.newFixedThreadPool(3); //固定大小线程池
+//        ExecutorService executorService = Executors.newCachedThreadPool(); // 缓存线程池(无大小限制)
+        ExecutorService executorService = Executors.newSingleThreadExecutor(); //单线程线程池(池中只有一个线程)
+        for (int i = 1; i <= 10; i++) {
+            final int task = i;//task是当前是第几个任务
+            executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+                    for (int j = 1; j <= 10; j++) {// j是当前任务执行的第几次
+                        System.out.println(Thread.currentThread().getName() + " is loop of " + j + " for task of " + task);
+                    }
+                }
+            });
+        }
+        System.out.println("all of 10 task has commited!");
+        executorService.shutdown();
+
+        // 2.定时线程池演示
+        //(1)创建一个定时线程,10s后执行任务
+//        Executors.newScheduledThreadPool(3).schedule(new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.println("bomb....!");
+//            }
+//        }, 10, TimeUnit.SECONDS);
+        
+        //(2)创建一个定时线程,6s后执行任务,之后每个2s重复执行
+        Executors.newScheduledThreadPool(3).scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("bomb...!");
+            }
+        },6,2,TimeUnit.SECONDS);
+    }
+}
